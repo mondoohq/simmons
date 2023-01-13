2c04e3a57c081e514a9cab487f49cca515bb0061
Ben Rockwood <benr@cuddletech.com>
2023-01-12 19:14:09 -0800

    Switch to headless operation
    Signed-off-by: Ben Rockwood <benr@cuddletech.com>

--- mondoo-us-test.py
+++ mondoo-us-test.py
@@ -3,7 +3,7 @@ import re
 import os
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://console.mondoo.com/")
