44f64bba19de1969ea8192452abd4615f77f516a
Ben Rockwood <benr@cuddletech.com>
2023-01-27 17:19:33 -0800

    Re-implement tests in TypeScript
    Signed-off-by: Ben Rockwood <benr@cuddletech.com>

---
+++ playwright.config.ts
@@ -0,0 +1,60 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'github',
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    //video: 'retain-on-failure',
+    video: 'on',
+    trace: 'retain-on-failure',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+  ],
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+ outputDir: 'results/',
+
+};
+
+export default config;
---
+++ tests/mondoo-console.spec.ts
@@ -0,0 +1,54 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+// Determine the desired region
+let region: string = 'US'
+if (process.env.REGION){
+        region = process.env.REGION;
+}
+
+
+test('Console Test', async ({ page }) => {
+  await page.goto('https://console.mondoo.com/');
+
+  // Phase 1: Login to US Default Space
+  await page.getByRole('button', { name: 'Sign in with email instead' }).click();
+  await page.getByLabel('Email').fill('ops+checkly@mondoo.com');
+  await page.getByLabel('Password').fill('kCSSicE#*MARoR@B^U7KTw#zqJpN6gM5');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page.getByRole('heading', { name: 'WELCOME TO MONDOO' })).toBeVisible;
+
+  // Phase 2: Click the logo to get the dashboard & Select Region 
+  await page.getByRole('link', { name: 'Mondoo Dashboard' }).click();
+  await page.getByRole('button', { name: 'US' }).click();
+
+  // Choose our region:
+  console.log('Region is:', region);
+  await page.getByRole('button', { name: region }).click();
+
+  // Phase 3: Navigate to Default Space and Find the Alpine Container Scan
+  await page.getByRole('link', { name: 'default Open assets' }).click();
+  await page.getByRole('tab', { name: 'Fleet' }).click();
+  await expect(page.getByRole('tab', { name: 'Settings' })).toBeVisible();
+
+  // Phase 4: Find Alpine Scan
+  await page.getByRole('cell', { name: 'alpine' }).nth(0).click();
+  // Check for a vulnerability count (should be 0 of 15 for Alpine Image)
+  await page.getByRole('tab', { name: 'Platform Vulnerabilities' }).click();
+  await expect(page.getByText('total')).toContainText('of 15');
+
+  // Phase 5: Delete the Asset and confirm
+  await page.getByRole('tab', { name: 'Fleet' }).click();
+  await page.getByRole('main').locator('button').nth(1).click();
+  await page.getByRole('row', { name: 'Asset Name Platform Score Last Updated' }).getByRole('checkbox').check();
+  await page.locator('#assets-batch-edit-selection').click();
+  await page.getByRole('option', { name: 'Delete' }).click();
+  await page.getByRole('button', { name: 'Done' }).click();
+  await page.getByRole('button', { name: 'Delete' }).click();
+  await expect(page.getByRole('heading', { name: 'WELCOME TO MONDOO' })).toBeVisible();
+
+  // Phase 6: Logout
+  await page.getByRole("button").nth(3).click();
+  await page.getByRole("menuitem", { name: 'Logout'}).click();
+
+});
